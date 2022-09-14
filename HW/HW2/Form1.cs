7cc273207caa6dace1718c3491a434dc16d9ebca
Aidan Griffin <aidan.griffin@wsu.edu>
2022-09-14 01:25:00 -0700

    Added unit testing file.

--- HW/HW2/Form1.cs
+++ HW/HW2/Form1.cs
@@ -5,6 +5,18 @@ namespace HW2
         public Form1()
         {
             InitializeComponent();
+            RunDistinctIntegers();
+        }
+
+        private static void RunDistinctIntegers()
+        {
+            var rand = new Random();
+            var list = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                int num = rand.Next(20000);
+                list.Add(num);
+            }
         }
     }
 }
