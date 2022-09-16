911685485123e6a24f3f0a8ed8b13a1ffb2ecc49
Aidan Griffin <aidan.griffin@wsu.edu>
2022-09-15 19:34:44 -0700

    Implemented questions 2 / 3

--- HW/HW2/Form1.cs
+++ HW/HW2/Form1.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Net.Security;
 using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Threading;
 
 namespace HW2NS
 {
     public partial class Form1 : Form
     {
+        private int hashCount = 0;
+        private int storageCount = 0;
+        private int sortedCount = 0;
+
+
         public Form1()
         {
             InitializeComponent(); // Load built in WinForms features
@@ -14,7 +22,11 @@ namespace HW2NS
             RunDistinctIntegers(passedList);
         }
 
-        public static List<int> GenerateRandomList()
+        /// <summary>
+        ///  A method that generates a 10,000 member list of random integers between 1-20,000.
+        /// </summary>
+        /// <returns> Returns list of random integers </returns>
+        public List<int> GenerateRandomList()
         {
             var rand = new Random();
             var randList = new List<int>();
@@ -27,11 +39,11 @@ namespace HW2NS
             return randList;
         }
 
-        /* 
-         * Function determines how many distinct integers are in a list of 10,000 random integers
-         * It takes three seperate approaches and returns output information.
-         */
-        private void RunDistinctIntegers(List<int> randList)
+        /// <summary>
+        ///  Function determines how many distinct integers are in a list of 10,000 random integers
+        ///  It takes three seperate approaches and returns output information.
+        /// </summary>
+        public void RunDistinctIntegers(List<int> randList)
         {
             HashSet<int> randHash = new HashSet<int>();
             for (int i = 0; i < randList.Count; i++)
@@ -39,9 +51,75 @@ namespace HW2NS
                 randHash.Add(randList[i]);
             }
 
-            this.textBox1.AppendText("HashSet Method: " + randHash.Count);
+            this.hashCount = randHash.Count;
+
+            for (int index = 1; index < randList.Count; index++)
+            {
+                int nestedIndex = 0;
+
+                for (nestedIndex = 0; nestedIndex < index; nestedIndex++)
+                {
+                    if (randList[index] == randList[nestedIndex])
+                    {
+                        break;
+                    }
+                }
+
+                if (index == nestedIndex)
+                {
+                    this.storageCount += 1;
+                }
+            }
+
+            List<int> sortedList = randList;
+            sortedList.Sort();
+
+            int j = -1;
+
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                if (i != j)
+                {
+                    this.sortedCount++;
+                    j = i;
+                }
+            }
+
+            this.textBox1.AppendText("HashSet Method: " + this.hashCount);
             this.textBox1.AppendText(Environment.NewLine);
             this.textBox1.AppendText("Time Complexity: O");
+            this.textBox1.AppendText(Environment.NewLine);
+            this.textBox1.AppendText("O(1) Storage Method: " + this.storageCount);
+            this.textBox1.AppendText(Environment.NewLine);
+            this.textBox1.AppendText("Sorted Method: " + this.sortedCount);
+
+        }
+
+        /// <summary>
+        /// Getter returns count value from hashing problem.
+        /// </summary>
+        /// <returns> Number of distinct integers from hashing problem. </returns>
+        public int GetHashCount()
+        {
+            return this.hashCount;
+        }
+
+        /// <summary>
+        /// Getter returns count from O(1) storage problem.
+        /// </summary>
+        /// <returns> Number of distinct integers from storage problem. </returns>
+        public int GetStorageCount()
+        {
+            return this.storageCount;
+        }
+
+        /// <summary>
+        /// Getter returns count from sorted problem.
+        /// </summary>
+        /// <returns> Number of distinct images derived from sorted problem. </returns>
+        public int GetSortedCount()
+        {
+            return this.sortedCount;
         }
     }
 }
--- HW/HW2Tests/UnitTest1.cs
+++ HW/HW2Tests/UnitTest1.cs
@@ -8,8 +8,11 @@ namespace HW2Tests
         public void Setup()
         {
         }
-        
-        
+
+        /// <summary>
+        /// Test which passes the distinct integer function a list of zeros. 
+        /// Since all elements passed will be 0, there should only be 1 distinct integer.
+        /// </summary>
         [Test]
         public void TestDistinctIntegersMethodWithListOfZeroes()
         {
@@ -19,7 +22,33 @@ namespace HW2Tests
             {
                 testList.Add(0);
             }
-            Form1.;
+
+            Form1 testForm = new Form1();
+
+            testForm.RunDistinctIntegers(testList);
+            int hashCount = testForm.GetHashCount();
+            Assert.That(hashCount, Is.EqualTo(1));
+        }
+
+        /// <summary>
+        /// Test which passes a list of sorted, non-repeating integers from 0-10000 into the 10000 member list.
+        /// Since all elements passed are distinct, there should be 10000 disitinct integers.
+        /// </summary>
+        [Test]
+        public void TestDistinctIntegersMethodWithListOfInorderIntegers()
+        {
+            var testList = new List<int>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                testList.Add(i);
+            }
+
+            Form1 testForm = new Form1();
+
+            testForm.RunDistinctIntegers(testList);
+            int hashCount = testForm.GetHashCount();
+            Assert.That(hashCount, Is.EqualTo(10000));
         }
     }
 }
