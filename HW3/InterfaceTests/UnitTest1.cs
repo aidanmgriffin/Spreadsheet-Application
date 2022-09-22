4f95d24d94a45baa3762b91f2e63c88479df7f79
Aidan Griffin <aidan.griffin@wsu.edu>
2022-09-22 13:52:36 -0700

    Created test file to begin implementing non-gui work.

---
+++ HW3/InterfaceTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace NotepadApplicationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
