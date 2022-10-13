f0679e4614b4a8c2ad1c88e155b3e5b2956d0d61
Aidan Griffin <aidan.griffin@wsu.edu>
2022-10-13 11:43:58 -0700

    Add cellchild to create instance of abstract cell class.

---
+++ Project/SpreadsheetEngine/CellChild.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetEngine
+{
+    public class CellChild : Cell
+    {
+        public CellChild(int columnIndex, int rowIndex) : base(columnIndex, rowIndex)
+        { 
+
+        }
+
+    }
+}
