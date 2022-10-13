608a04e484e57241ef803361de45d67b28e72fbd
Aidan Griffin <aidan.griffin@wsu.edu>
2022-10-13 11:43:02 -0700

    Create cell class

---
+++ Project/SpreadsheetEngine/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace SpreadsheetEngine
+{
+    public abstract class Cell : INotifyPropertyChanged
+    {
+
+        public Cell(int rowIndex, int columnIndex)
+        {
+            this.rowIndex = rowIndex;
+            this.columnIndex = columnIndex;
+        }
+
+        public int rowIndex { get; }
+        public int columnIndex { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected string cellText
+        {
+            get { return cellText; }
+            set
+            {
+                if (value == cellText) { return; }
+                cellText = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("cellText"));
+            }
+        }
+
+        protected string cellValue
+        {
+            get { return cellValue; }
+        }
+    }
+}
